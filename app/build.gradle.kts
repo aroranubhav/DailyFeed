fe0f6d547b96edb86a9e1e49d1d120763cc6b370
maxi <arora.anubhav93@gmail.com>
2025-11-11 14:56:35 +0530

    Add dependencies and setup api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,29 +1,47 @@
+import java.util.Base64
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "com.maxi.dailyfeed"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.maxi.dailyfeed"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    val properties = Properties().apply {
+        rootProject.file("local.properties").takeIf { it.exists() }?.reader()?.use(::load)
+    }
+
+    val apiKey = properties["API_KEY"] ?: error("Error: API KEY not found!")
+    val encodedKey = Base64.getEncoder().encodeToString(apiKey.toString().toByteArray())
+
     buildTypes {
+        all {
+            buildConfigField("String", "API_KEY", "\"$encodedKey\"")
+        }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isMinifyEnabled = false
         }
     }
     compileOptions {
@@ -32,6 +50,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -42,6 +64,47 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    //swipe refresh layout
+    implementation(libs.androidx.swipe.refresh)
+
+    //lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.runtime)
+
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+
+    //room
+    implementation(libs.room.ktx)
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+
+    //interceptor
+    implementation(libs.logging.interceptor)
+
+    //browser
+    implementation(libs.browser)
+
+    //glide
+    implementation(libs.glide)
+
+    //worker + hilt-work
+    implementation(libs.work.runtime)
+    implementation(libs.hilt.work)
+    ksp(libs.hilt.work.compiler)
+
+    //datastore
+    implementation(libs.data.store)
+
+    //paging
+    implementation(libs.androidx.paging)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
