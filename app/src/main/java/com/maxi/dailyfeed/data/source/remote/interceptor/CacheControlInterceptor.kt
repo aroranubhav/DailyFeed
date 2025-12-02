fae3fe64677a5226557480a3ab9d8d47d671d860
maxi <arora.anubhav93@gmail.com>
2025-12-02 18:18:24 +0530

    Add cache control interceptor

---
+++ app/src/main/java/com/maxi/dailyfeed/data/source/remote/interceptor/CacheControlInterceptor.kt
@@ -0,0 +1,42 @@
+package com.maxi.dailyfeed.data.source.remote.interceptor
+
+import com.maxi.dailyfeed.data.source.common.DataConstants.Headers.X_FORCE_REFRESH
+import okhttp3.CacheControl
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.util.concurrent.TimeUnit
+
+class CacheControlInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
+        val forceRefreshHeader = request.header(X_FORCE_REFRESH) == "true"
+
+        return if (request.method == "GET") {
+            val modifiedRequest = when {
+                forceRefreshHeader -> {
+                    request
+                        .newBuilder()
+                        .removeHeader(X_FORCE_REFRESH)
+                        .cacheControl(CacheControl.FORCE_NETWORK)
+                        .build()
+                }
+
+                else -> {
+                    request
+                        .newBuilder()
+                        .cacheControl(
+                            CacheControl.Builder()
+                                .maxAge(2, TimeUnit.MINUTES)
+                                .build()
+                        )
+                        .build()
+                }
+            }
+            chain.proceed(modifiedRequest)
+        } else {
+            chain.proceed(request)
+        }
+    }
+}
