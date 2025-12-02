421eba48b6307505abc13b01d9318f4324e64c9d
maxi <arora.anubhav93@gmail.com>
2025-12-02 18:18:42 +0530

    Add custom qualifiers for DI

---
+++ app/src/main/java/com/maxi/dailyfeed/framework/di/qualifier/qualifiers.kt
@@ -0,0 +1,21 @@
+package com.maxi.dailyfeed.framework.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+annotation class ApiKey
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+annotation class IsDebug
+
+
+
