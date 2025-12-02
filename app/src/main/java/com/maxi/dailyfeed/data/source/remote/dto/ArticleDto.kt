d9c1aeb7784cf21b243ecbbca056a54b86204764
maxi <arora.anubhav93@gmail.com>
2025-12-02 18:09:59 +0530

    Add data classes
    Classes use kotlinx serialization

---
+++ app/src/main/java/com/maxi/dailyfeed/data/source/remote/dto/ArticleDto.kt
@@ -0,0 +1,19 @@
+package com.maxi.dailyfeed.data.source.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import com.maxi.dailyfeed.data.source.common.DataConstants.Keys as Keys
+
+@Serializable
+data class ArticleDto(
+    @SerialName(Keys.TITLE)
+    val title: String,
+    @SerialName(Keys.DESCRIPTION)
+    val description: String?,
+    @SerialName(Keys.URL)
+    val url: String,
+    @SerialName(Keys.IMAGE_URL)
+    val imageUrl: String?,
+    @SerialName(Keys.PUBLISHED_AT)
+    val publishedAt: String?
+)
---
+++ app/src/main/java/com/maxi/dailyfeed/data/source/remote/dto/NewsResponseDto.kt
@@ -0,0 +1,15 @@
+package com.maxi.dailyfeed.data.source.remote.dto
+
+import com.maxi.dailyfeed.data.source.common.DataConstants.Keys as Keys
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NewsResponseDto(
+    @SerialName(Keys.STATUS)
+    val status: String,
+    @SerialName(Keys.TOTAL_RESULTS)
+    val totalResults: Int,
+    @SerialName(Keys.ARTICLES)
+    val articles: List<ArticleDto>
+)
