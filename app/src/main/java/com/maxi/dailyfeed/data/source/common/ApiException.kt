c7146736a983ebc1b202260a4d9a034ea99afdc8
maxi <arora.anubhav93@gmail.com>
2025-12-02 18:15:18 +0530

    Add custom Exception wrapper for API-related IO errors

---
+++ app/src/main/java/com/maxi/dailyfeed/data/source/common/ApiException.kt
@@ -0,0 +1,29 @@
+package com.maxi.dailyfeed.data.source.common
+
+import java.io.IOException
+
+open class ApiException(
+    val errorCode: Int,
+    val errorMessage: String?,
+    val errorBody: String?,
+    val requestMethod: String?,
+    val requestUrl: String?
+) : IOException() {
+
+    override val message: String
+        get() = buildString {
+            append("HTTP Exception $errorCode")
+
+            if (!errorMessage.isNullOrEmpty()) {
+                append(" -- $errorMessage")
+            }
+
+            if (!requestMethod.isNullOrEmpty() && !requestUrl.isNullOrEmpty()) {
+                append("-- [$requestMethod -- $requestUrl]")
+            }
+
+            if (!errorBody.isNullOrEmpty()) {
+                append("\n $errorBody")
+            }
+        }
+}
