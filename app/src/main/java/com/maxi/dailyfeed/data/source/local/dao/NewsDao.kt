9533771de4d3b5fa385ca80121412e1a6dd179d6
maxi <arora.anubhav93@gmail.com>
2025-12-07 02:08:18 +0530

    Add news DAO methods

---
+++ app/src/main/java/com/maxi/dailyfeed/data/source/local/dao/NewsDao.kt
@@ -0,0 +1,26 @@
+package com.maxi.dailyfeed.data.source.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.maxi.dailyfeed.data.source.local.entity.ArticleEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NewsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNews(articles: List<ArticleEntity>): List<Long>
+
+    @Query(
+        """
+        SELECT * FROM articles
+        ORDER BY publishedAt IS NULL, publishedAt
+    """
+    )
+    fun getNews(): Flow<List<ArticleEntity>>
+
+    @Query("DELETE FROM articles")
+    suspend fun clearNews()
+}
