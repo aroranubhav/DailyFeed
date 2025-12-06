28a8b8ad6015952461e0a362ef6438ff4fca2fd8
maxi <arora.anubhav93@gmail.com>
2025-12-07 02:30:32 +0530

    Add a safe api and db methods class to handle different responses

---
+++ app/src/main/java/com/maxi/dailyfeed/data/source/common/SafeCall.kt
@@ -0,0 +1,65 @@
+package com.maxi.dailyfeed.data.source.common
+
+import androidx.sqlite.SQLiteException
+import com.maxi.dailyfeed.common.ErrorType
+import com.maxi.dailyfeed.common.Resource
+import kotlinx.coroutines.CancellationException
+import java.io.IOException
+
+suspend fun <T> safeApiCall(apiCall: suspend () -> T): Resource<T> {
+    return try {
+        val response = apiCall()
+        Resource.Success(response)
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: ApiException) {
+        val errorType = when (e.errorCode) {
+            401 -> ErrorType.UNAUTHORISED
+            else -> ErrorType.API
+        }
+        Resource.Error(
+            errorType,
+            e.errorMessage
+        )
+    } catch (e: IOException) {
+        Resource.Error(
+            ErrorType.IO,
+            e.message
+        )
+    } catch (e: Exception) {
+        Resource.Error(
+            ErrorType.UNKNOWN,
+            e.message
+        )
+    }
+}
+
+suspend fun <T> safeDbCall(dbCall: suspend () -> T): Resource<T> {
+    return try {
+        val response = dbCall()
+        Resource.Success(response)
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: SQLiteException) {
+        Resource.Error(
+            ErrorType.DATABASE,
+            e.message
+        )
+    } catch (e: IllegalStateException) {
+        Resource.Error(
+            ErrorType.DATABASE,
+            e.message
+        )
+    } catch (e: IOException) {
+        Resource.Error(
+            ErrorType.IO,
+            e.message
+        )
+    } catch (e: Exception) {
+        Resource.Error(
+            ErrorType.UNKNOWN,
+            e.message
+        )
+    }
+}
+
