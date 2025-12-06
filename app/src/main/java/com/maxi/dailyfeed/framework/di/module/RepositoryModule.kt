29a55e5708f0ceeb1af567df383c54b872df82b0
maxi <arora.anubhav93@gmail.com>
2025-12-07 02:58:23 +0530

    Add DI provider method for app datastore implementation

--- app/src/main/java/com/maxi/dailyfeed/framework/di/module/RepositoryModule.kt
+++ app/src/main/java/com/maxi/dailyfeed/framework/di/module/RepositoryModule.kt
@@ -1,12 +1,16 @@
 package com.maxi.dailyfeed.framework.di.module
 
+import android.content.Context
 import com.maxi.dailyfeed.data.repository.DefaultNewsRepository
 import com.maxi.dailyfeed.data.source.local.LocalDataSource
 import com.maxi.dailyfeed.data.source.remote.RemoteDataSource
+import com.maxi.dailyfeed.domain.repository.AppDataStore
 import com.maxi.dailyfeed.domain.repository.NewsRepository
+import com.maxi.dailyfeed.framework.datastore.DefaultAppDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -21,4 +25,11 @@ object RepositoryModule {
         local: LocalDataSource
     ): NewsRepository =
         DefaultNewsRepository(remote, local)
+
+    @Provides
+    @Singleton
+    fun provideAppDataStore(
+        @ApplicationContext context: Context
+    ): AppDataStore =
+        DefaultAppDataStore(context)
 }
