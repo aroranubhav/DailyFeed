2ab6fff3073508c8f74d3550f2296c30d70d794f
maxi <arora.anubhav93@gmail.com>
2025-12-07 02:07:24 +0530

    Update getNews interface to include force refresh parameter

--- app/src/main/java/com/maxi/dailyfeed/data/source/remote/api/NetworkApiService.kt
+++ app/src/main/java/com/maxi/dailyfeed/data/source/remote/api/NetworkApiService.kt
@@ -1,9 +1,11 @@
 package com.maxi.dailyfeed.data.source.remote.api
 
 import com.maxi.dailyfeed.data.source.common.DataConstants.EndPoints.TOP_HEADLINES
+import com.maxi.dailyfeed.data.source.common.DataConstants.Headers.X_FORCE_REFRESH
+import com.maxi.dailyfeed.data.source.common.DataConstants.QueryParams
 import com.maxi.dailyfeed.data.source.remote.dto.NewsResponseDto
-import com.maxi.dailyfeed.data.source.common.DataConstants.QueryParams as QueryParams
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface NetworkApiService {
@@ -11,6 +13,7 @@ interface NetworkApiService {
     @GET(TOP_HEADLINES)
     suspend fun getNews(
         @Query(QueryParams.LANGUAGE) language: String,
-        @Query(QueryParams.COUNTRY) country: String
+        @Query(QueryParams.COUNTRY) country: String,
+        @Header(X_FORCE_REFRESH) forceRefresh: Boolean = false
     ): NewsResponseDto
 }
