040b7c57638c5895952d994a7cab902290b4e8cd
maxi <arora.anubhav93@gmail.com>
2025-12-07 16:03:30 +0530

    Add dispatcher provider interface and it's implementation

---
+++ app/src/main/java/com/maxi/dailyfeed/common/DispatcherProvider.kt
@@ -0,0 +1,24 @@
+package com.maxi.dailyfeed.common
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+interface DispatcherProvider {
+
+    val main: CoroutineDispatcher
+
+    val default: CoroutineDispatcher
+
+    val io: CoroutineDispatcher
+}
+
+class DefaultDispatcherProvider: DispatcherProvider {
+    override val main: CoroutineDispatcher
+        get() = Dispatchers.Main
+
+    override val default: CoroutineDispatcher
+        get() = Dispatchers.Default
+
+    override val io: CoroutineDispatcher
+        get() = Dispatchers.IO
+}
