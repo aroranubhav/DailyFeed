4568f584fa691142489ee002d3be63b6a7bb5321
maxi <arora.anubhav93@gmail.com>
2025-12-08 15:48:06 +0530

    Update Resource file
    This commit adds errorCode in the Error data class which is later used to check if work needs to be retried based on the errorCode

--- app/src/main/java/com/maxi/dailyfeed/common/Resource.kt
+++ app/src/main/java/com/maxi/dailyfeed/common/Resource.kt
@@ -5,8 +5,9 @@ sealed class Resource<out T> {
     data class Success<T>(val data: T) : Resource<T>()
 
     data class Error(
-        val type: ErrorType,
-        val message: String?
+        val errorType: ErrorType,
+        val errorMessage: String? = null,
+        val errorCode: Int? = null
     ) : Resource<Nothing>()
 
     data object Loading : Resource<Nothing>()
