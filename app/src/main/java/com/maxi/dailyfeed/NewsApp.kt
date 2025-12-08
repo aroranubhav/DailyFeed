fdb062d90151e389f4ca65b6509556dd1dcc0758
maxi <arora.anubhav93@gmail.com>
2025-12-08 15:51:26 +0530

    Update NewsApp class
    This commit adds custom work manager configuration, and calls work scheduler

--- app/src/main/java/com/maxi/dailyfeed/NewsApp.kt
+++ app/src/main/java/com/maxi/dailyfeed/NewsApp.kt
@@ -1,7 +1,36 @@
 package com.maxi.dailyfeed
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.WorkManager
+import com.maxi.dailyfeed.framework.work.NewsRefreshScheduler
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class NewsApp: Application()
+class NewsApp : Application(),
+    Configuration.Provider {
+
+    @Inject
+    lateinit var hiltWorkerFactory: HiltWorkerFactory
+
+    @Inject
+    lateinit var workManager: WorkManager
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration
+            .Builder()
+            .setWorkerFactory(hiltWorkerFactory)
+            .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        NewsRefreshScheduler
+            .scheduleWork(
+                workManager,
+                "en",
+                "us"
+            )
+    }
+}
