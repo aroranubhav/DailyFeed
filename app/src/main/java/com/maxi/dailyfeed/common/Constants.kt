0255bea295c0d3ec4ee46d64873ca0b254764447
maxi <arora.anubhav93@gmail.com>
2025-12-09 13:19:19 +0530

    Add constants

--- app/src/main/java/com/maxi/dailyfeed/common/Constants.kt
+++ app/src/main/java/com/maxi/dailyfeed/common/Constants.kt
@@ -4,4 +4,9 @@ object Constants {
 
     const val LANGUAGE = "language"
     const val COUNTRY = "country"
+
+    const val NEWS_REFRESH_NOTIFICATION_CHANNEL_ID = "news_refresh_notification_channel"
+    const val NEWS_REFRESH_NOTIFICATION_NAME = "news_refresh_notifications"
+    const val NEWS_NOTIFICATION_TITLE = "NEWS"
+    const val NEWS_NOTIFICATION_TEXT = "Your updated feed!"
 }
