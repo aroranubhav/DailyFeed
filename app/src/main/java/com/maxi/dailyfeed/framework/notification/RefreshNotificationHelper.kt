6bde33c3fff7cd28a344f551d6061eb6c302f421
maxi <arora.anubhav93@gmail.com>
2025-12-09 15:36:02 +0530

    Update RefreshNotificationHelper
    This commit adds a pending intent to launch the NewsActivity on click of the notification

--- app/src/main/java/com/maxi/dailyfeed/framework/notification/RefreshNotificationHelper.kt
+++ app/src/main/java/com/maxi/dailyfeed/framework/notification/RefreshNotificationHelper.kt
@@ -1,9 +1,12 @@
 package com.maxi.dailyfeed.framework.notification
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.maxi.dailyfeed.R
+import com.maxi.dailyfeed.presentation.news.ui.NewsActivity
 import com.maxi.dailyfeed.common.Constants as Constants
 import javax.inject.Inject
 
@@ -17,10 +20,21 @@ class RefreshNotificationHelper @Inject constructor(
     }
 
     fun showNewsRefreshedNotification() {
-
         if (!permissionChecker.isPostNotificationPermissionGranted()) {
             return
         }
+
+        val intent = Intent(context, NewsActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent
+            .getActivity(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
 
         val notification = NotificationCompat.Builder(
             context,
@@ -30,6 +44,7 @@ class RefreshNotificationHelper @Inject constructor(
             .setContentTitle(Constants.NEWS_NOTIFICATION_TITLE)
             .setContentText(Constants.NEWS_NOTIFICATION_TEXT)
             .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
             .build()
 
         NotificationManagerCompat.from(
