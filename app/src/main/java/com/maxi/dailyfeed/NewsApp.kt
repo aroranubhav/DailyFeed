36d9e239fe1b33e5e19fd30b97e7b6c16e699798
maxi <arora.anubhav93@gmail.com>
2025-12-09 13:17:28 +0530

    Updated NewsApp
    This commit adds a notification channel for refresh news notification

--- app/src/main/java/com/maxi/dailyfeed/NewsApp.kt
+++ app/src/main/java/com/maxi/dailyfeed/NewsApp.kt
@@ -1,9 +1,14 @@
 package com.maxi.dailyfeed
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import androidx.work.WorkManager
+import com.maxi.dailyfeed.common.Constants.NEWS_REFRESH_NOTIFICATION_CHANNEL_ID
+import com.maxi.dailyfeed.common.Constants.NEWS_REFRESH_NOTIFICATION_NAME
 import com.maxi.dailyfeed.framework.work.NewsRefreshScheduler
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -32,5 +37,19 @@ class NewsApp : Application(),
                 "en",
                 "us"
             )
+        createNewsRefreshNotificationChannel()
+    }
+
+    private fun createNewsRefreshNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NEWS_REFRESH_NOTIFICATION_CHANNEL_ID,
+                NEWS_REFRESH_NOTIFICATION_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
     }
 }
