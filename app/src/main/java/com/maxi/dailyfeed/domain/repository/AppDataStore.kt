7520633e51901c1a4e1ce2b4c9b7f2529f49067b
maxi <arora.anubhav93@gmail.com>
2025-12-12 19:50:17 +0530

    Update AppDataStore
    Update AppDataStore to add reading and writing ETag for /top-headlines GET request

--- app/src/main/java/com/maxi/dailyfeed/domain/repository/AppDataStore.kt
+++ app/src/main/java/com/maxi/dailyfeed/domain/repository/AppDataStore.kt
@@ -5,4 +5,8 @@ interface AppDataStore {
     suspend fun getIsAlreadyLaunched(): Boolean
 
     suspend fun updateIsAlreadyLaunched()
+
+    suspend fun getETag(): String?
+
+    suspend fun saveETag(value: String)
 }
