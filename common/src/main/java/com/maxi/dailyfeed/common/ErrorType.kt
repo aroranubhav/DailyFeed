64f0bd29dec68934c0e6b4989108f4f09d640fce
maxi <arora.anubhav93@gmail.com>
2026-03-22 20:33:04 +0530

    Add different ErrorType enum class
    This class adds possible common error types(IO, HTTP and Database)

---
+++ common/src/main/java/com/maxi/dailyfeed/common/ErrorType.kt
@@ -0,0 +1,13 @@
+package com.maxi.dailyfeed.common
+
+enum class ErrorType {
+
+    NO_CONNECTIVITY,
+    TIMEOUT,
+    AUTHORISATION,
+    FORBIDDEN,
+    NOT_FOUND,
+    SERVER_ERROR,
+    DATABASE,
+    UNKNOWN
+}
