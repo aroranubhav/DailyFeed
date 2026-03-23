af6eba6bd71b41881194e652ae6a0388430d65be
maxi <arora.anubhav93@gmail.com>
2026-03-23 18:20:23 +0530

    Update Resource sealed class for UI state management
    Introduced a generic Resource wrapper to represent Success, Error, Loading, and NoChange states. This standardizes how the Repository communicates results to the View layer.

--- common/src/main/java/com/maxi/dailyfeed/common/Resource.kt
+++ common/src/main/java/com/maxi/dailyfeed/common/Resource.kt
@@ -2,13 +2,18 @@ package com.maxi.dailyfeed.common
 
 sealed class Resource<out T> {
 
-    data class Success<T>(val data: T): Resource<T>()
+    data class Success<T>(
+        val data: T,
+        val syncMetaData: DBSyncMetaData? = null
+    ) : Resource<T>()
 
     data class Error(
         val type: ErrorType,
         val message: String?,
-        val code: Int?
-    ): Resource<Nothing>()
+        val code: Int? = null
+    ) : Resource<Nothing>()
 
-    data object Loading: Resource<Nothing>()
+    data object Loading : Resource<Nothing>()
+
+    data object NoChange : Resource<Nothing>()
 }
