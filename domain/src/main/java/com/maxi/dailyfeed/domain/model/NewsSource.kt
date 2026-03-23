de386e1295e308ae70e5cfef4fce987110452ed6
maxi <arora.anubhav93@gmail.com>
2026-03-23 18:05:02 +0530

    Update domain models

--- domain/src/main/java/com/maxi/dailyfeed/domain/model/NewsSource.kt
+++ domain/src/main/java/com/maxi/dailyfeed/domain/model/NewsSource.kt
@@ -3,6 +3,6 @@ package com.maxi.dailyfeed.domain.model
 data class NewsSource(
     val id: String,
     val name: String,
-    val description: String?,
-    val url: String?
+    val description: String,
+    val url: String
 )
