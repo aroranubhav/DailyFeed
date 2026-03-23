2fd35209758efc0018805bc70dd6ad9dddc1c8cd
maxi <arora.anubhav93@gmail.com>
2026-03-23 21:18:32 +0530

    Add Serializable annotation to the DTO class

--- data/src/main/java/com/maxi/dailyfeed/data/source/remote/dto/NewsSourceDto.kt
+++ data/src/main/java/com/maxi/dailyfeed/data/source/remote/dto/NewsSourceDto.kt
@@ -1,7 +1,9 @@
 package com.maxi.dailyfeed.data.source.remote.dto
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class NewsSourceDto(
     @SerialName("id")
     val id: String,
